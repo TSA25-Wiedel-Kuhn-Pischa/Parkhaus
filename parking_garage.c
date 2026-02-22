7f70fd0fdcc6cc4f2167534d88ca0def2c2b59fd
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-22 21:15:16 +0100

    Include matching header file and functions

--- parking_garage.c
+++ parking_garage.c
@@ -1,1 +1,39 @@
+/*
+ *  File: parking_garage.c
+ *  Description: Functions used in relation to the parking-garage
+ * 
+ */
 
+#include "parking_garage.h"
+
+struct car *create_garage(int parking_spaces){
+  /*
+  
+  */
+}
+
+void park_car(struct car){
+  /*
+  
+  */
+}
+
+void remove_car(struct car){
+  /*
+  
+  */
+}
+
+void check_parking_time(struct car *parking_garage){
+  /*
+  
+  */
+}
+
+void manage_cars(struct car *parking_garage){
+  /*
+  check_parking_time(parking_garage);
+  park_cars();
+  remove_cars();
+  */
+}
