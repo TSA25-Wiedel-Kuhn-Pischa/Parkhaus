248a9834b9e7d8c11ba84cd9519b2b680ad2d3e7
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-24 12:51:49 +0100

    edit check_parking_time-function

--- parking_garage.c
+++ parking_garage.c
@@ -9,25 +9,34 @@
 
 struct car *create_garage(int parking_spaces){
   /*
-  
+  struct Car *car = calloc(parking_spaces, sizeof(car));
   */
 }
 
 void park_car(struct Car car){
   /*
-  
+  store specific car struct in non-occupied array-slots
   */
 }
 
 void remove_car(struct Car car){
   /*
-  
+  remove car from array
+  remove specific car struct
   */
 }
 
-void check_parking_time(struct car *parking_garage){
+void check_parking_time(struct car *parking_garage, int array_size, int time_step){
   /*
-  
+  FOR i <- 0 TO array_size DO
+    IF (parking_garage[i] != NULL) THEN
+      i++
+    ELSE
+      IF (time_step-(parking_garage[i]->arrival_time) == parking_garage[i]->parking_duration) THEN
+        parking_garage[i]->parking_duration_reached = 1       //set flag to true: maximum parking duration is reached
+      END IF
+    END IF
+  END FOR
   */
 }
 
