352171468cda16f0c6dc783c2ff715b7319c190d
Marcel <kuehn.marcel-it25@it.dhbw-ravensburg.de>
2026-02-25 20:36:50 +0100

    defined functions

--- statistics/reapeted_statistics.h
+++ statistics/reapeted_statistics.h
@@ -21,20 +21,6 @@
 
 /**
 
-    @brief Es wird berechnet, wie viele Parkplätze noch frei sind
-
-    Es wird die Anzahl aller Parkplätze minus die Anazhl der belegten Parkplätze gerechnet.
-
-    @param[in] occupied, Eingabe von der Anzahl der besetzten Parkplätze 
-    @param[in] all, Eingabe von der Anazhl aller Parkplätze 
-
-    @return Es wird der berechnete Wert zurück gegeben
-*/
-
-//int Function free(int occupied, int all);
-
-/**
-
     @brief Es wird berechnet, wie viele Autos seit dem letzten Zeitpunkt dazu gekommen sind 
 
     Es wird der Wert, die Anzahl der Autos, des vorherigen Aufrufes gespeichert und von der jetzigen Anzahl der Autos abgezogen.
@@ -50,54 +36,97 @@
 
     @brief Es werden in der Konsole die aktuellen Daten ausgegeben
 
-    Es werden die berechneten Werte, fullnes und freie Parkplätze, sowie die Anzahl der Autos mit dem aktuellen 
-    Zeitschritt in der Konsole ausgegeben
+    Es wird der berechnete Werte von der Funktion fullnes, sowie die Anzahl der Autos im Parkhaus, in der möglichen 
+    Warteschlange und Insgesamt ausgegeben, und auch die Anzahl der freien Parkplätze.
+    All dies wird mit dem aktuellen Zeitschritt in der Konsole ausgegeben
 
     @param[in] occupied, Eingabe von der Anzahl der besetzten Parkplätze 
     @param[in] all, Eingabe von der Anazhl aller Parkplätze
+    @param[in] cars_in_line, Eingabe von der Anazhl aller Autos in der Warteschlange
 
     @return Ausgabe von den aktuellen Werten 
 */
 
-//int Function output_data(int occupied, int all);
+//void Function output_data(int occupied, int all, int cars_in_line);
 
 /**
 
-    @brief Es werden in der Konsole die aktuellen Daten ausgegeben
+    @brief Es werden die Daten von dem aktuellen Zeitpunkt gespeichert
 
-    Es werden die berechneten Werte, fullnes und freie Parkplätze, sowie die Anzahl der Autos mit dem aktuellen 
-    Zeitschritt gespeichert 
+    Es werden die berechneten Werte, fullnes und freie Parkplätze, sowie die Anzahl der Autos im Parkhaus, 
+    in der möglichen Warteschlange und Insgesamt mit dem aktuellen Zeitschritt gespeichert 
 
     @param[in] occupied, Eingabe von der Anzahl der besetzten Parkplätze 
     @param[in] all, Eingabe von der Anazhl aller Parkplätze
+    @param[in] cars_in_line, Eingabe von der Anazhl aller Autos in der Warteschlange
+
+    @return Rückgabe von dem erstellten Array, in dem die Werte gespeichert werden
 */
 
-//int Function save_data(int occupied, int all);
+//int Function save_data(int occupied, int all, int cars_in_line);
+
+/**
+
+    @brief Es wird jeweils der höchste Wert ausgegeben
+
+    ...
+
+    @param[in] Array, Eingabe von dem Array aus der Funktion save_data()
+
+    @return Es wird in der Kosole ausgegeben und in einem externen Dokument dargestellt 
+*/
+
+//int Function out_maxval(int Array[]);
 
 /**
 
     @brief Es wird eine Tabelle in der Konsole ausgegeben 
 
-    Es wird, wenn die Funktion das erste mal aufgerufen wird, eine Tabelle erstellt. Es wird der Zeitpunkt des Aufrufens 
-    dargestellt, sowie in den Zeilen dadrunter die Angabe der Füllmenge, Anzahl der freien Parkplätze, die Anzahl der 
-    Autos, sowie die Rate der hinein- und hinuasfahrenden Fahrzeuge. Nach dem ersten Aufruf wird für jeden Aufrauf, eine 
-    neue Spalte dazu geschrieben. Dies wird in der Konsole ausgegeben.
+    Es wird eine Tabelle erstellt, welche 10 Spalten und 8 Zeilen hat. 
+    Es wird die Anzahl der gesamten Simulationsschritte durch 10 geteilt.
+    Dadurch erhält man den Absatand der einzelnen Schritte. Mit diesen gff. gerundeten Abständen 
+    wird von 0 bis zum letzten Simulatinsschritt durgegangen. 
+    In diesem Abständen wird die Tabelle geliedert. 
+    Als Wert dargestellt werdem soll die Füllmenge, die Anzahl der Autos insgesamt, im Parkhaus und in der Wartschlange,
+    sowie die Anzahl der freien Parkplätze und Rate.
+    Um die Werte zu Erhalten, werden die Daten aus dem Array von der Funktion save_data(), ausgelesen. Um Rate zu erhälten
+    wird mit der Anazhl der Autos die Funktion rate() aufgerufen. 
 
-    @param[in] occupied, Eingabe von der Anzahl der besetzten Parkplätze 
-    @param[in] all, Eingabe von der Anazhl aller Parkplätze
+    @param[in] Array, Eingabe von dem Array aus der Funktion save_data()
 
-    @return Es wird eine Tabelle in der Kosole ausgegeben
+    @return Es wird in der Kosole ausgegeben und in einem externen Dokument dargestellt 
 */
 
-//int Function tabel(int occupied, int all);
+//int Function tabel(int Array[]);
 
 /**
 
     @brief Es wird ein Säulendiagram in der Konsole ausgegeben 
 
-    Es wird, wenn die Funktion das erste mal aufgerufen wird, ein Säulendiagram erstellt. Es wird der Zeitpunkt des Aufrufens 
-    auf der X-Achse dargestellt und auf der Y-Achse wird die Prozentanzahl dargestellt (Maximal 100%). 
-    Nach dem ersten Aufruf wird für jeden Aufrauf, eine neue Säule ergänzt, um den Verlauf sehen zu können.
+    Es wird ein Säulendiagram erstellt. Auf der X-Achse werden 10 Zeitpunkte dargestellt, diese werden ermittelt, 
+    indem die Anzahl der gesamten Simulationsschritte durch 10 geteilt wird. Dadurch erhält man den Absatand der einzelnen 
+    Schritte. Mit diesen gff. gerundeten Abständen wird von 0 bis zum letzten Simulatinsschritt durgegangen. 
+    Auf der Y-Achse wird von 0 bis 100 in Prozent skaliert. 
+    Als Wert pro Zeitschritt wird die Füllmenge dargestellt, welche aus dem Array von der Funktion save_data() entnommen wird.
+
+    @param[in] Array, Eingabe von dem Array aus der Funktion save_data()
+
+    @return Es wird ein Säulendiagram in der Konsole Ausgegeben und einem externen Dokument dargestellt
+*/
+
+//int Function column_chart(int Array[]);
+
+/**
+
+    @brief Es wird ein Balkendiagram in der Konsole ausgegeben 
+
+    Es wird ein Balkendiagram erstellt. Auf der Y-Achse werden 10 Zeitpunkte dargestellt, diese werden ermittelt, 
+    indem die Anzahl der gesamten Simulationsschritte durch 10 geteilt wird. Dadurch erhält man den Absatand der einzelnen 
+    Schritte. Mit diesen gff. gerundeten Abständen wird von 0 bis zum letzten Simulatinsschritt durgegangen. 
+    Auf der X-Achse wird skaliert von 0 bis zum höchsten Wert von der Anzahl der Autos in der Warteschlange.
+    Als Wert pro Zeitschritt wird die Anzahl der Autos in der Warteschlange dargestellt, 
+    welche aus dem Array von der Funktion save_data() entnommen werden.
+
 
     @param[in] occupied, Eingabe von der Anzahl der besetzten Parkplätze 
     @param[in] all, Eingabe von der Anazhl aller Parkplätze 
@@ -106,6 +135,6 @@
     @return Es wird ein Säulendiagram in der Konsole Ausgegeben
 */
 
-//int Function column_chart(int occupied, int all, int time);
+//int Function bar_chart(int occupied, int all, int time);
 
 //#endif
