b72c1d07837d4fb3ec1ab4e7270382b95774b695
Marcel <kuehn.marcel-it25@it.dhbw-ravensburg.de>
2026-03-01 13:52:45 +0100

    started working on creating parameter_input

---
+++ include/parameter.h
@@ -0,0 +1,29 @@
+/**  
+    Datei: parameter.h
+    Beschreibung: Zusammenfasung aller Funktionen zur Eingabe der Parameter.
+*/
+
+//#ifndef PARAMETER
+//#define PARAMETER
+
+
+/**
+
+*
+*    @brief Es wird eine Eingabemöglichkeit der Fuktionsparameter aufgerufen
+*
+*    Im Terminal wird für die jeweiligen Werte eine Abfrage gestellt gestellt. 
+*    Dabei wird auch auf Fehleingaben überprüft und die Werte werden übergeben 
+*
+*    @param[in] *spaces, Eingabe Anzahl der Stellplätze
+*    @param[in] *max_parking, Eingabe Maximale Parkdauer
+*    @param[in] *size, Eingabe Simulationsdauer (Zeitschritte)
+*    @param[in] *chance_of_new_cars, Eingabe Ankunftswahrscheinlichkeit neuer Fahrzeuge in %
+*    @param[in] *seed, Eingabe Zufalls-Seed für reprudzierbare Simulation
+*
+*    @return Es wird garnichts zurückgegeben, da void funktion. Die Werte werden über ihre Adressen übergeben 
+*/
+
+//void FUNCTION input(int *spaces, int *max_parking, int *size, int *chance_of_new_cras, int *seed);
+
+//#endif
