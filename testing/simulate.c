f5ab8c3d011fdbeee487a193bf18b51285edba38
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-03-02 12:42:57 +0100

    WORKED IN GROUP: start working on main

--- testing/simulate.c
+++ testing/simulate.c
@@ -7,9 +7,37 @@
 /*
 int main(){
 
-  Initialisierung eines car-struct-Pointers mit dem Aufruf von create_garage()
+  FILE *daten = fopen("../src/Daten.txt", "w")
+  FILE *auswertung = fopen("../src/Auswertung.txt", "w")
+  IF (daten == NULL || auswertung == NULL) DO
 
-  
+    OUTPUT "Fehler beim Öffnen mindestens einer Datei."
+    return 1
 
+  END IF
+
+
+  //Initialisieren von Simulationsparametern
+  int spaces = 0
+  float max_parking = 0
+  int size = 0
+  float chance_of_new_cars = 0
+  int seed = 0
+
+  //Einlesen und Setzen der Simulationsparameter
+  input_parameters(&spaces, &max_parking, &size, &chance_of_new_cars, &seed)
+
+  int data[(size*5)] = {0}
+
+
+  Random Seed aus Simulationsparameter setzen
+
+  //For schleife:
+    //Überprüfung von Autos im Parkhaus und entsprechendes Ausparken
+    //Erstellung von Autos und Fahren in die Warteschlange
+    //Einparken von Autos ins Parkhaus, falls Plätze frei sind
+    //Ausgabe von Statistiken
+
+  //Ausgabe von Endstatistiken
 }
 */
