39828c5e239e96c09931b8cfa19e08c19a2f4367
floriantechnik <253711083+floriantechnik@users.noreply.github.com>
2026-03-03 12:36:11 +0100

    move explanation from .c to .h

--- include/cars.h
+++ include/cars.h
@@ -21,6 +21,34 @@ typedef struct car{
     char modelname[50];
 } car;
 
+
 int create_car(int max_parking_duration, int time_stemp, struct car* c, struct queue* queue1);
+/**
+    * @brief setzt die Werte eines Car-Structs
+    *        und parkt das Auto oder setzt es in die Warteschlange
+    *
+    * @param[in]  time_stemp  aktueller Zeitschritt, zu dem das Auto ankommt
+    * @param[in]  struct car* c  Pointer auf Car-Struct, dass bearbeitet wird
+    * @param[in]  max_parking_duration maximale Parkdauer
+    * @param[in]  *queue1  Zeiger auf die Warteschlange
+    * 
+    * return   nur zur Fehlersignalisierung, wenn die Funktion -1 zurückgibt, ist ein Fehler aufgetreten
+*/
+
 int car_id_counter();
+/**
+    * @brief zählt die IDs für die Autos hoch, damit jedes Auto eine eindeutige ID bekommt
+    *
+    * @param[out] counter aktuelle ID, die zurückgegeben wird, wenn die Funktion aufgerufen wird
+    * @return            gibt counter zurück
+*/
+
 int randomize_parking_duration(int max_parking_duration);
+/**
+    * @brief gibt eine zufällige Parkdauer zurück
+    *
+    * @param[in]  r_max_parking_duration maximale Parkdauer
+    * @param[in]  r_time_step_size Zeitschrittgröße für die Berechnung der Parkdauer
+    * @param[out] random_parking_duration zufällige Parkdauer
+    * @return            gibt random_parking_duration zurück
+*/
