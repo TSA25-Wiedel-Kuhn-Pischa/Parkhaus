f6125ddd8226d764cfb84accdf0a955d89be7866
floriantechnik <253711083+floriantechnik@users.noreply.github.com>
2026-03-11 18:09:27 +0100

    more tests for cars.c and queue.c

--- testing/test.c
+++ testing/test.c
@@ -34,6 +34,15 @@ int main(void) {
     assert(test_car_two.brand[0] != '\0');                      //Marke überprüfen
     assert(test_car_two.modelname[0] != '\0');                  //Modellname überprüfen
 
+    //falsche Inputs für create_car()
+    struct car test_car_three;
+    assert(create_car(80, -1, &test_car_three, test_queue) == -1);
+    struct car test_car_four;
+    assert(create_car(-5, 50, &test_car_four, test_queue) == -1);
+    struct car test_car_five;
+    assert(create_car(80, -1, &test_car_five, NULL) == -1);
+    assert(create_car(83, 50, NULL, test_queue) == -1);
+
     //Tests für car_id_counter()
     int id1 = car_id_counter();
     int id2 = car_id_counter();
@@ -58,13 +67,14 @@ int main(void) {
 
     //Tests für queue_get_size()
     assert(queue_get_size(test_queue2) == 0);
-    struct car test_car_five;
-    assert(queue_enqueue(test_queue2, &test_car_five) == 0);
+    struct car test_car_six;
+    assert(queue_enqueue(test_queue2, &test_car_six) == 0);
     assert(queue_get_size(test_queue2) == 1);
     assert(queue_get_size(NULL) == -1);
 
     //Tests für queue_enqueue()
-    assert(queue_enqueue(NULL, NULL) == -1);
+    assert(queue_enqueue(NULL, &test_car_six) == -1);
+    assert(queue_enqueue(test_queue2, NULL) == -1);
 
  return 0; //Tests bestanden
 }
