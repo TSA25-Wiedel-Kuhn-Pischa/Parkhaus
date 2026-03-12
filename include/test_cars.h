a33b5b4836785433319f2b51bec22385f52077a6
floriantechnik <253711083+floriantechnik@users.noreply.github.com>
2026-03-12 14:37:41 +0100

    splitt unit tests into more c files

---
+++ include/test_cars.h
@@ -0,0 +1,27 @@
+#ifndef TEST_CARS_H
+#define TEST_CARS_H
+
+#ifndef ASSERT_H
+    #define ASSERT_H
+    #include <assert.h>
+#endif
+
+//Alle eigenen include Dateien einbinden
+#include "../include/cars.h"
+#include "../include/parameter.h"
+#include "../include/parking_garage.h"
+#include "../include/queue.h"
+#include "../include/statistics.h"
+
+
+int test_cars(void);
+/**
+* @brief unit tests for cars.c
+*
+*
+* @param[out] 0 Wenn ohne Fehler durchgelaufen
+
+* @return 0 Wenn ohne Fehler durchgelaufen
+*/
+
+#endif
