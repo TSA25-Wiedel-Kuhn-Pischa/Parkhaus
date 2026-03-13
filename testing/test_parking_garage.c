b1c15bba336e8c08fdef992a7a7d1766fddef876
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-03-13 17:38:11 +0100

    add source file for testing the parking-garage

---
+++ testing/test_parking_garage.c
@@ -0,0 +1,16 @@
+#ifdef DEBUG
+ 
+printf("Debug-Modus aktiv\n");
+
+#include <assert.h>
+#include "../include/parking_garage.h"
+
+int test_queue()
+{
+//Unit-Tests für das Parkhaus:
+    
+
+    return 0; //Tests erfolgreich
+}
+
+#endif
