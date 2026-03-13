361ac72ef710f40d6ebba2931397e1eac4688460
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-03-13 19:31:18 +0100

    create header file for test_parking_garage

---
+++ include/test_parking_garage.h
@@ -0,0 +1,16 @@
+#ifndef TEST_PARKING_GARAGE_H
+#define TEST_PARKING_GARAGE_H
+
+#include "../include/cars.h"
+#include "../include/queue.h"
+#include "../include/parking_garage.h"
+
+/*
+ * @brief  Unit-Tests der Funktionalität der Funktionen in parking_garage.c
+ *
+ * @param[in]   parking_spaces    Anzahl der Parkplätze des Parkhauses (hier nur für die Testzwecke)
+ * @param[out]  integer           Rückgabewert, ob der Testdurchlauf erfolgreich war
+ */
+int test_parking_garage(int parking_spaces);
+
+#endif
