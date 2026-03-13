fe3a88aad68e3647591e8863686478f57935a3ae
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-03-13 19:08:35 +0100

    further working on testing of parking-garage-functions

--- testing/test_parking_garage.c
+++ testing/test_parking_garage.c
@@ -6,32 +6,66 @@ int test_parking_garage(int parking_spaces){
  
   //Tests für create_garage():
   car **ptr_parking_garage = create_garage(parking_spaces);
-  assert(ptr_parking_garage != NULL);                       //Überprüfung, ob das Parkhaus korrekt erstellt wurde
-  assert(ptr_parking_garage == NULL || ptr_parking_garage != NULL);       //Überprüfung, ob die Funktion nicht definierte Werte zurückgibt
+  assert(ptr_parking_garage != NULL);                                 //Überprüfung, ob das Parkhaus korrekt erstellt wurde
+  assert(ptr_parking_garage == NULL || ptr_parking_garage != NULL);   //Überprüfung, ob die Funktion nicht definierte Werte zurückgibt
 
-  car test_car;
+  car test_car;                                                       //Test-Auto für die Testzwecke
   ptr_parking_garage[0] = &test_car;
-  assert(ptr_parking_garage[0] == &test_car);               //Überprüfung, ob Pointer auf Car-Struct in dem Parkhaus gespeichert werden können
+  assert(ptr_parking_garage[0] == &test_car);                         //Überprüfung, ob Pointer auf Car-Struct in dem Parkhaus gespeichert werden können
 
 
   //Tests für get_free_space();
   assert(get_free_space(ptr_parking_garage, parking_spaces) != -1);   //Überprüfung, ob der Funktionsaufruf erfolgreich ist
 
-  for(int i=0; i<parking_spaces; i++){    //Parkhaus wird komplett befüllt
+  for(int i=0; i<parking_spaces; i++){                                //Parkhaus wird komplett mit dem gleichen Testauto befüllt
     ptr_parking_garage[i] = &test_car;
   }
   assert(get_free_space(ptr_parking_garage, parking_spaces) == -1);   //Überprüfung, ob die Funktion einen Fehler zurückgibt, da der Aufruf
                                                                       //dieser Funktion bei vollem Parkhaus garnicht erst möglich sein sollte
-  
-  ptr_parking_garage[0] = NULL;     //ein Parkplatz (hier Index 0) wird freigemacht
+  ptr_parking_garage[0] = NULL;                                       //ein Parkplatz (hier Index 0) wird freigemacht
   assert(get_free_space(ptr_parking_garage, parking_spaces) == 0);    //Überprüfung, ob die Funktion den richtigen Rückgabewert liefert
 
 
   //Tests für park_car():
-  int time_step = 1;    //fiktiver Simulationswert als Funktionsparameter; ist für den Test nicht von Relevanz
-  assert(park_car(test_car, ptr_parking_garage, parking_spaces, time_step) == 0);
+  int time_step = 1;                                                  //fiktiver Simulationswert als Funktionsparameter; ist für den Test nicht von Relevanz
+  car test_car2;                                                      //Weiteres Test-Auto zur Unterscheidung von den anderen bereits geparkten Autos
+  assert(park_car(&test_car2, ptr_parking_garage, parking_spaces, time_step) == 0); //Überprüfung der korrekten Ausführung der Funktion
+  assert(ptr_parking_garage[0] != NULL);                              //Überprüfung, ob das Auto korrekt geparkt wurde
+  assert(ptr_parking_garage[0] = &test_car2);
+  assert(test_car2.arrival_time = time_step);                         //Überprüfung, ob die korrekte Ankunftszeit übergeben wurde
 
 
+  //Tests für remove_car():
+  remove_car(&test_car2, ptr_parking_garage, 0);                      //Entferne das Test-Auto test_car2 aus dem Parkhaus
+  assert(ptr_parking_garage[0] == NULL);                              //Überprüfung, ob der zuvor belegte Parkplatz nun frei ist
+  assert(&test_car2 == NULL);                                         //Überprüfung, ob der Pointer auf das Auto auf NULL gesetzt wurde
+
+
+  //Tests für check_parking_time():
+  car test_car3;
+  test_car3.parking_duration = 3;
+  test_car3.arrival_time = 5;
+  time_step = 8;
+  assert(check_parking_time(&test_car3, time_step) == 1);             //Test bei erreichter maximaler Parkdauer
+
+  //Überprüfung, ob die Funktion einen Fehlerwert zurückgibt
+  assert(check_parking_time(&test_car3, time_step) == 1 || check_parking_time(&test_car3, time_step) == 0);
+  
+  test_car3.arrival_time = 7;
+  assert(check_parking_time(&test_car3, time_step) == 0);             //Test bei nicht-erreichter maximaler Parkdauer
+
+
+  //Tests für check_for_free_space():
+  assert(check_for_free_space(ptr_parking_garage, parking_spaces) == 0);  //Überprüfung bei vollem Parkhaus -> Rückgabewert muss 0 sein
+
+  for(int i=0; i<parking_spaces; i++){                                //Parkhaus wird komplett geleert
+    ptr_parking_garage[i] = NULL;
+  }
+  assert(check_for_free_space(ptr_parking_garage, parking_spaces) == parking_spaces); //Überprüfung bei leerem Parkhaus -> Rückgabewert muss Anzahl der Parkplätze sein
+  
+
+
+  free(ptr_parking_garage);                                           //Freigabe des für das Parkhaus reservierten Speichers
 
   return 0; //Tests erfolgreich
 }
