d61c5672adf6800811a095f5cefce2aa610a0286
Marcel <kuehn.marcel-it25@it.dhbw-ravensburg.de>
2026-03-14 17:04:46 +0100

    added test_statistics()

--- testing/test_statistics.c
+++ testing/test_statistics.c
@@ -2,3 +2,11 @@
     Datei: test_statistics.h
     Beschreibung: Beschreibung aller Funktionen zum Testen von statistics.c
 */
+#include "../include/test_statistics.h"
+#include "../include/statistics.h"
+
+int test_statistics()
+{
+
+
+}
