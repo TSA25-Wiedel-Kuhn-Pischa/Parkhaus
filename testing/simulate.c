c6b1e415119a2ac03af0c751de794a45b34c9348
floriantechnik <253711083+floriantechnik@users.noreply.github.com>
2026-03-16 14:16:50 +0100

    edit fileheaders

--- testing/simulate.c
+++ testing/simulate.c
@@ -1,7 +1,7 @@
 /*
  *  File: simulate.c
  *  Description: In simulate.c werden die Funktionen der src-Dateien zusammengeführt, um eine Simulation
- *               und das Testen der Funktionen zu ermöglichen.
+ *  und das Testen der Funktionen zu ermöglichen.
  */
 
 
